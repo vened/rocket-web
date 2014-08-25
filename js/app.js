205c79cacbc3666512ed3dbae1266c5e4d9a250c
Stolbukhin Maksim <stolbukhin@mamba.ru>
2014-08-25 15:39:02 +0400

    makeup

--- js/app.js
+++ js/app.js
@@ -1,33 +1,12 @@
-$(function () {
-    // Navbar
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $(".navbar.navbar-fixed-top").addClass("scroll");
-        } else {
-            $(".navbar.navbar-fixed-top").removeClass("scroll");
-        }      
-    });
+$(document).ready(function ()
+{
 
-    // scroll back to top btn
-    $('.scrolltop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 700);
-        return false;
-    });
-    
-    // scroll navigation functionality
-    $('.scroller').click(function(){
-    	var section = $($(this).data("section"));
-    	var top = section.offset().top;
-        $("html, body").animate({ scrollTop: top }, 700);
-        return false;
-    });
-
-    // Index project carrousel
-	$('.flexslider').flexslider({
-		directionNav: false,
-		animation: "slide",
-		controlNav: true,
-		pauseOnHover: true
+	var navWrap = $(".js-nav-wrap"),
+		nav = navWrap.find(".js-nav"),
+		navExpand = navWrap.find(".js-nav-expand");
+	
+	navExpand.on('click', function(){
+		nav.slideToggle();
 	});
-
+	
 });
